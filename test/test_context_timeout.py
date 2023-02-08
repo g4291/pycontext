9dced722dce16a5d06acf49a29ef5f15487c59b7
g4291 <george@4291.cz>
2023-02-08 12:31:31 +0100

    timeout tests

--- test/test_context_timeout.py
+++ test/test_context_timeout.py
@@ -3,20 +3,34 @@
 import time
 import logging
 
-from context import ContextWithTimeout
+from context import ContextWithTimeout, ContextTimeoutError
 
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-def test_thread(timeout: float):
-    with ContextWithTimeout(timeout, True):
+def run_tests():
+    
+    print("Test 1, timeout 3s, suppress exception")
+    with ContextWithTimeout(3, True):
         while True:
             print("Worker runs")
             time.sleep(1)
 
-def run_tests():
-    test_thread(3)
+    print("Test 2, timeout 3s, raise exception")
+    try:
+        with ContextWithTimeout(3):
+            while True:
+                print("Worker runs")
+                time.sleep(1)
+    except ContextTimeoutError as e:
+        print("Timeout")
+
+    print("Test 3, no timeout")
+    with ContextWithTimeout(0):
+        time.sleep(1)
+        print("Bye")
+
 
 if __name__ == "__main__":
     run_tests()
